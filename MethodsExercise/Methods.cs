4163eff1cd0f388a777ad2f9b0108e6bbe264c4d
Demitri Cameron <oblivion9794@gmail.com>
2022-11-30 10:11:52 -0500

    I think I'm done lol

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class Methods
+
+    {
+        //private static int items;
+
+        public static int Add(int num1, int num2)
+        {
+
+            int sum = num1 + num2;
+            return sum;
+
+        }
+
+        public static void Multiply(int num1, int num2)
+        {
+
+            var answer = num1 * num2;
+            Console.WriteLine($"{num1 * num2} = {answer}");
+
+    }
+
+        public static int Subtract(int num1, int num2)
+        {
+
+            int sum = num1 - num2;
+            return sum;
+
+
+        }
+        public static int Divide(int num1, int num2)
+        {
+
+            int sum = num1 / num2;
+            return sum;
+
+        }
+
+        public static void AddWithParam(params int[] numbers)
+        {
+
+            int sum = 0;
+            foreach (var num in numbers)
+            {
+                sum = sum + num;
+        }
+            Console.WriteLine(sum);
+        }
+
+
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, New User. Welcome to Exercise 1.");
+
+
+
+            Console.WriteLine("What is your name?"); // output
+            var name = Console.ReadLine(); // input
+
+            Console.WriteLine("Where are you from?"); // output
+            Console.ReadLine(); // input
+
+            Console.WriteLine("What is one of your favorite foods?"); // output
+            Console.ReadLine(); // input
+
+            Console.WriteLine("What do you like about it?"); // output
+            Console.ReadLine(); // input
+
+            Console.WriteLine("Is it something you can get nearby?"); // output
+            Console.ReadLine(); // input
+
+            Console.WriteLine("Thank you for your time."); // output
+
+            ////-----------------Ex 2-----------------------------
+            ////Method Calls
+
+            Console.Write(Methods.Add(3, 7));
+            Methods.Multiply(3, 7);
+            var answer = Methods.Subtract(20, 10);
+            Console.WriteLine($"20 - 10 = {answer}");
+
+            //Console.ReadLine();
+
+            Methods.AddWithParam(5, 5, 5, 5, 5, 5, 5);
+
+            //I THINK this was everything required for the exercise.
+            //I haven't uploaded it because I wanted to truly understand methods before doing so. 
+            //I understood this assignment since it was something simple like Addition, Subtraction, etc.
+
         }
     }
 }
